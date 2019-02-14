bb618a80816fb604862f30b0756e2d3ecc3f4192
Jacopo Antonello <jacopo@antonello.org>
2019-02-14 10:23:17 +0000

    Back port control improvements

--- dmlib/control.py
+++ dmlib/control.py
@@ -12,14 +12,17 @@ class ZernikeControl:
 
     saturation = 0
 
-    def __init__(self, dm, calib, indices=None, h5f=None):
-        self.log = logging.getLogger('ZernikeControl')
+    def __init__(self, dm, calib, args=None, h5f=None):
+        self.args = args
+        self.log = logging.getLogger(self.__class__.__name__)
 
         nz = calib.H.shape[0]
         nu = calib.H.shape[1]
 
-        if indices is None:
+        if args is None:
             indices = np.arange(1, nz + 1)
+        else:
+            indices = get_noll_indices(args)
         assert(calib.get_rzern().nk == nz)
         ndof = indices.size
 
@@ -42,33 +45,60 @@ class ZernikeControl:
         self.z1 = np.zeros((nz,))
         self.ab = np.zeros((ndof,))
         self.u = np.zeros((nu,))
+        self.u0 = np.zeros((nu,))
 
         self.flat_on = 1
 
         if h5f:
             calib.save_h5py(h5f)
 
-        def make_empty(name, shape, dtype=np.float):
-            h5f.create_dataset(
+        if h5f:
+            self.h5_make_empty('flat_on', (1,), np.bool)
+            self.h5_make_empty('x', (ndof,))
+            self.h5_make_empty('u', (nu,))
+
+        self.h5_save('name', self.__class__.__name__)
+        self.h5_save('ab', self.ab)
+        self.h5_save('P', np.eye(nz))
+
+    def h5_make_empty(self, name, shape, dtype=np.float):
+        if self.h5f:
+            name = 'ZernikeControl/' + name
+            if name in self.h5f:
+                del self.h5f[name]
+            self.h5f.create_dataset(
                 name, shape + (0,), maxshape=shape + (None,),
                 dtype=dtype)
-        if h5f:
-            make_empty('ZernikeControl/flat_on', (1,), np.bool)
-            make_empty('ZernikeControl/x', (ndof,))
-            make_empty('ZernikeControl/u', (nu,))
-
-        self.h5_save('ab', self.ab)
-        self.h5_save('P', np.eye(nz, nz))
 
     def h5_append(self, name, what):
         if self.h5f:
+            name = 'ZernikeControl/' + name
             self.h5f[name].resize((
                 self.h5f[name].shape[0], self.h5f[name].shape[1] + 1))
             self.h5f[name][:, -1] = what
 
     def h5_save(self, where, what):
         if self.h5f:
-            self.h5f['ZernikeControl/' + where] = what
+            name = 'ZernikeControl/' + where
+            if name in self.h5f:
+                del self.h5f[name]
+            self.h5f[name] = what
+
+    def u2z(self):
+        # for GUI purposes & does not include flat
+        # z1 = P*z
+        # u = C*z1
+        # z1 = H*u
+        if self.flat_on:
+            tmp = self.u - self.calib.uflat
+        else:
+            tmp = self.u
+        tmp += self.u0
+        z1 = np.dot(self.calib.H, tmp)
+        if self.P is None:
+            return z1
+        else:
+            return np.dot(self.P.T, z1)
 
     def write(self, x):
         assert(x.shape == self.ab.shape)
@@ -82,10 +112,12 @@ class ZernikeControl:
         if self.flat_on:
             self.u += self.calib.uflat
 
+        self.u += self.u0
+
         if self.h5f:
-            self.h5_append('ZernikeControl/flat_on', self.flat_on)
-            self.h5_append('ZernikeControl/x', x)
-            self.h5_append('ZernikeControl/u', self.u)
+            self.h5_append('flat_on', self.flat_on)
+            self.h5_append('x', x)
+            self.h5_append('u', self.u)
 
         if norm(self.u, np.inf) > 1:
             self.log.warn(
@@ -126,13 +158,27 @@ class ZernikeControl:
         if tot.size == 1:
             return
         else:
-            if self.P is None:
-                self.P = tot
-            else:
-                np.dot(tot, self.P.copy(), self.P)
+            self.set_P(tot)
 
-        if self.h5f:
-            self.h5f['P'][:] = self.P[:]
+    def set_P(self, P):
+        if P is None:
+            self.P = None
+
+            if self.h5f:
+                del self.h5f['P']
+                self.h5f['P'][:] = np.eye(self.nz)
+        else:
+            assert(P.ndim == 2)
+            assert(P.shape[0] == P.shape[1])
+            assert(np.allclose(np.dot(P, P.T), np.eye(P.shape[0])))
+            if self.P is None:
+                self.P = P.copy()
+            else:
+                np.dot(P, self.P.copy(), self.P)
+
+            if self.h5f:
+                del self.h5f['P']
+                self.h5f['P'][:] = self.P[:]
 
     def make_rot_matrix(self, alpha):
         cz = self.calib.get_rzern()
@@ -190,6 +236,10 @@ class ZernikeControl:
         assert(norm((np.dot(R.T, R) - np.eye(cz.nk)).ravel()) < 1e-11)
         return R
 
+    def store_u0(self, u):
+        self.u0[:] = u
+        self.h5_save('u0', self.u0)
+
 
 def get_noll_indices(args):
     if args.noll_min > 0 and args.noll_max > 0:
@@ -197,13 +247,13 @@ def get_noll_indices(args):
     else:
         mrange = np.array([], dtype=np.int)
 
-    if args.noll_include is not None:
+    if args.noll_include != '':
         minclude = np.fromstring(args.noll_include, dtype=np.int, sep=',')
         minclude = minclude[minclude > 0]
     else:
         minclude = np.array([], dtype=np.int)
 
-    if args.noll_exclude is not None:
+    if args.noll_exclude != '':
         mexclude = np.fromstring(args.noll_exclude, dtype=np.int, sep=',')
         mexclude = mexclude[mexclude > 0]
     else:
@@ -219,16 +269,24 @@ def get_noll_indices(args):
     return zernike_indices
 
 
+control_types = {
+    'Zernike': ZernikeControl,
+    }
+
+
 def add_control_parameters(parser):
     parser.add_argument(
-        '--noll-include', type=str, default=None, metavar='INDICES',
-        help='Comma separated list of Noll indices to include')
+        '--noll-include', type=str, default='', metavar='INDICES',
+        help='Comma separated list of Noll indices to include, eg 1,2')
     parser.add_argument(
-        '--noll-exclude', type=str, default=None, metavar='INDICES',
-        help='Comma separated list of Noll indices to exclude')
+        '--noll-exclude', type=str, default='1,2,3,4', metavar='INDICES',
+        help='Comma separated list of Noll indices to exclude, eg 1,2')
     parser.add_argument(
         '--noll-min', type=int, default=5, metavar='MIN',
         help='Minimum Noll index to consider, use -1 to ignore')
     parser.add_argument(
         '--noll-max', type=int, default=6, metavar='MAX',
         help='Maximum Noll index to consider, use -1 to ignore')
+    parser.add_argument(
+        '--control', choices=list(control_types.keys()),
+        default=list(control_types.keys())[0], help='DM control type')
